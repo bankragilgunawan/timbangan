3d3c86ea94bb253e15e747b2fe806b337cf8cc9b
bankragilgunawan <bankragilgunawan@gmail.com>
2026-05-04 01:55:54 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,20 +4,18 @@ const ASSETS_TO_CACHE = [
   '/timbangan/index.html',
   '/timbangan/style.css',
   '/timbangan/script.js',
+  '/timbangan/1000032542.png',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
 
-// Tahap Instalasi
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Membuka cache dan menyimpan aset');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-// Strategi: Ambil dari Cache dulu, jika gagal baru ambil dari Network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
